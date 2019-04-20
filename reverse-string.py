19e3dcb248d13216a8221e8b47672269c1c873c2
Quadriphobs1 <abiodunquadriadekunle@gmail.com>
2019-04-20 16:26:18 +0100

    Pyton string

---
+++ reverse-string.py
@@ -0,0 +1,5 @@
+strings = "abcdefghjklmnopqrst"
+
+strings_reversed = strings[::-1]
+
+print(strings_reversed)
---
+++ str-loop.py
@@ -0,0 +1,9 @@
+s = "abcdefghi"
+
+for index in range(len(s)):
+    if s[index] == 'i' or s[index] == 'u':
+        print('There is an i or a u')
+
+for char in s:
+    if s[index] == 'i' or s[index] == 'u':
+        print('There is an i or a u')
